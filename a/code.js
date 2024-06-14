907eae50ff93a1bbb201508388ec1e61132aa18b
School Dev <112954598+Lucasyguyguyu@users.noreply.github.com>
2024-06-14 11:39:37 -0300

    Add files via upload

---
+++ a/code.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll(".obj");
+
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function () {
+                {
+            botoes[j].classList.add(".ativo");
+        }
+    }
+}
