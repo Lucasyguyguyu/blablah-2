488b6565dc656c64e946799e315aba0464ddb61a
School Dev <112954598+Lucasyguyguyu@users.noreply.github.com>
2024-06-24 10:51:25 -0300

    code 3.0
    code 3.0

--- a/code.js
+++ a/code.js
@@ -1,10 +1,16 @@
 const botoes = document.querySelectorAll(".obj");
+const texto = document.querySelectorAll(".frased")
 
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function () {
-                {
-            botoes[j].classList.add(".ativo");
+
+        for(let j=0;j <botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            texto[j].classList.remove("ativo");
         }
+            botoes[i].classList.add("ativo");
+            texto[i].classList.add("ativo");
     }
 }
+
